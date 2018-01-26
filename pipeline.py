f3e7ffdcbbbf63d1868c9fcdc26726d7ebbe1692
qcpwned07 <qcpwned07@gmail.com>
2018-01-26 01:27:48 +0100

    Create pipeline.py
    Here is what I tried to do, still not working tho :(

---
+++ pipeline.py
@@ -0,0 +1,94 @@
+import torch
+import torchvision.datasets
+import torchvision.transforms as transforms
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+
+#Create transforms for preprocessing
+trsf = transforms.Compose([ transforms.Resize((32,32)),
+        transforms.ToTensor()
+        ])
+#Load the small test set (only for overfitting the network)
+food_data = torchvision.datasets.ImageFolder('test/', trsf)
+
+data_loader = torch.utils.data.DataLoader(food_data,
+        batch_size=4,
+        shuffle=True,
+        num_workers=4)
+
+
+
+print (len(food_data))
+img, target = food_data[50]
+print("Image Size: ", img.size())
+
+
+
+num_epochs = 5
+batch_size = 100
+learning_rate = 0.001
+
+
+# CNN Model (2 conv layer)
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x): 
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+            
+cnn = CNN()
+
+
+# Loss and Optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(cnn.parameters(), lr=learning_rate)
+
+# Train the Model
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(data_loader):
+        images = Variable(images)
+        labels = Variable(labels)
+
+        # Forward + Backward + Optimize
+        optimizer.zero_grad()
+        outputs = cnn(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        if (i+1) % 100 == 0:
+            print ('Epoch [%d/%d], Iter [%d/%d] Loss: %.4f'
+                   %(epoch+1, num_epochs, i+1, len(train_dataset)//batch_size, loss.data[0]))
+
+# Test the Model
+cnn.eval()  # Change model to 'eval' mode (BN uses moving mean/var).
+correct = 0
+total = 0
+for images, labels in test_loader:
+    images = Variable(images)
+    outputs = cnn(images)
+    _, predicted = torch.max(outputs.data, 1)
+    total += labels.size(0)
+    correct += (predicted == labels).sum()
+
+print('Test Accuracy of the model on the 10000 test images: %d %%' % (100 * correct / total))
+
+# Save the Trained Model
+torch.save(cnn.state_dict(), 'cnn.pkl')
+
+
